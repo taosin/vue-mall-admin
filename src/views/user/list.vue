18d67c72694bf1171d767cf9abe077454bbb933e
Nix <xinxintao@foxmail.com>
2019-07-01 22:35:50 +0800

    add user-list

---
+++ src/views/user/list.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="user-list">
+       daafffjfajkflsjfj
+    </div>
+</template>
+<script>
+export default {
+    name: 'user-list',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="less">
+.user-list{
+    width: 100%;
+    height: 100%;
+    background-color: red;
+}
+</style>
