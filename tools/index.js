d395c9b785dafdf930e44a126a4530e6bbc3d396
Nix <xinxintao@foxmail.com>
2019-09-03 23:24:59 +0800

    create page

--- tools/index.js
+++ tools/index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-fs.readFile(__dirname+'/template.vue', 'utf-8',(err, data) => {
-    fs.writeFileSync(__dirname + "/page.vue", data, {flag:'a',encoding:'utf-8',mode:'0666'})
-});
+const path = require('path');
+let srcPath = path.resolve('./src');
+
 // 人机交互
 var question = [{
     type: 'list',
@@ -34,8 +34,25 @@ function runCMD(cmd) {
 
 function init(type, name) {
     if (name == '') {
-        return console.log('\n 名称不能为空');
+        console.log('\n 名称不能为空');
+        return;
     }
 
-    name = name.substring(0,1).toUpperCase()+name.substring(1)
+    let [folder_name, file_name] = name.split(' ')
+    console.log(folder_name, file_name);
+    let folder_path = `${srcPath}/views/${folder_name}`
+    let is_exist_folder = fs.existsSync(folder_path) || false;
+    if (!is_exist_folder) {
+        fs.mkdirSync(folder_path);
+    }
+    let file_path = `${folder_path}/${file_name}.vue`
+    let is_exist_file = fs.existsSync(file_path) || false;
+    if (is_exist_file) {
+        console.error("\n  该页面已存在");
+        return;
+    }
+    fs.readFile(__dirname + '/template.vue', 'utf-8', (err, data) => {
+        fs.writeFileSync(`${folder_path}/${file_name}.vue`, data, { flag: 'a', encoding: 'utf-8', mode: '0666' });
+    });
+    console.log('\x1b[36m%s\x1b[0m', "\n  页面" + file_name + "完成初始化！");
 }
