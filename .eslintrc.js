3cbe157105e28545ca79c1a0c0b234ca7a09acec
Nix <xinxintao@foxmail.com>
2019-09-05 19:17:09 +0800

    eslint init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    extends: [
-        'eslint-config-alloy/vue',
-    ],
-    globals: {
-        // 这里填入你的项目需要的全局变量
-        // 这里值为 false 表示这个全局变量不允许被重新赋值，比如：
-        //
-        // Vue: false
-    },
-    rules: {
-        // 这里填入你的项目需要的个性化配置，比如：
-        //
-        // // @fixable 一个缩进必须用两个空格替代
-        // 'indent': [
-        //     'error',
-        //     2,
-        //     {
-        //         SwitchCase: 1,
-        //         flatTernaryExpressions: true
-        //     }
-        // ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
